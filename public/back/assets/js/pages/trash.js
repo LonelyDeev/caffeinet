04a32b303b6efcf80c523587fa51c8f6cb1c5281
LonelyDeev <mahdi77yousefi2015@gmail.com>
2026-09-13 01:21:48 +0330

    first commit

--- public/back/assets/js/pages/trash.js
+++ public/back/assets/js/pages/trash.js
@@ -129,7 +129,7 @@
             '</div>';
 
         this.runModal(html, {
-            '[data-trash-cancel]': () => {},
+            '[data-trash-cancel]': () => {modal.remove()},
             '[data-trash-ok]': async (modal) => {
                 const okBtn = modal.querySelector('[data-trash-ok]');
                 okBtn.disabled = true;
